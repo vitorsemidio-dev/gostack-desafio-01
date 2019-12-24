177c739539ba92172ce3c8a0df580c6eb7c44922
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-24 15:23:47 -0300

    routes CRUD

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/projects', (req, res) => {
-  return res.send({ ok: 'sucesso' })
-})
+  return res.send({ ok: 'sucesso get' })
+});
+
+server.get('/projects/:id', (req, res) => {
+  return res.send({ ok: 'sucesso get :id' })
+});
+
+server.post('/projects', (req, res) => {
+  return res.send({ ok: 'sucesso post' })
+});
+
+server.put('/projects/:id', (req, res) => {
+  return res.send({ ok: 'sucesso put' })
+});
+
+server.delete('/projects/:id', (req, res) => {
+  return res.send({ ok: 'sucesso delete' })
+});
+
 
 server.listen(3000);
